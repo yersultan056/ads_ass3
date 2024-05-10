1e2fb3f7a9994be03908aac910eeb06c0c6ed0d4
yersik <230129@astanait.edu.kz>
2024-05-10 10:47:00 +0600

    1

---
+++ src/Insertion.java
@@ -0,0 +1,13 @@
+public class Insertion {
+    public static void insertionSort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int current = array[i];
+            int j = i;
+            while (j > 0 && array[j - 1] > current) {
+                array[j] = array[j - 1];
+                j--;
+            }
+            array[j] = current;
+        }
+    }
+}
---
+++ src/Quick.java
@@ -0,0 +1,37 @@
+public class Quick {
+
+    public static void quickSort(int[] arr, int from, int to) {
+        if (from < to) {
+            int divideIndex = partition(arr, from, to);
+            quickSort(arr, from, divideIndex - 1);
+            quickSort(arr, divideIndex, to);
+        }
+    }
+
+    private static int partition(int[] arr, int from, int to) {
+        int rightIndex = to;
+        int leftIndex = from;
+
+        int pivot = arr[from + (to - from) / 2];
+        while (leftIndex <= rightIndex) {
+            while (arr[leftIndex] < pivot) {
+                leftIndex++;
+            }
+            while (arr[rightIndex] > pivot) {
+                rightIndex--;
+            }
+            if (leftIndex <= rightIndex) {
+                swap(arr, rightIndex, leftIndex);
+                leftIndex++;
+                rightIndex--;
+            }
+        }
+        return leftIndex;
+    }
+
+    private static void swap(int[] array, int index1, int index2) {
+        int tmp  = array[index1];
+        array[index1] = array[index2];
+        array[index2] = tmp;
+    }
+}
